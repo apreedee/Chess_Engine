a5350254879d342908f2541f213c4516b61fee30
apreedee <afridimusab@gmail.com>
2023-12-15 14:47:08 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def board():
+    def __init__(fen):
+        self.fen = fen
+
+
+if __name__ == "__main__":
+    x = board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    print(x.fen)
